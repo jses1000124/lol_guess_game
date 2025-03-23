a5336afe143bfca2ea927cf17327e7a49abdf0e1
XiaoGuai <jses0922737039@gmail.com>
2025-03-23 18:38:06 +0800

    version 0.0.1

---
+++ script.js
@@ -0,0 +1,71 @@
+let champions = [];
+
+async function loadChampions() {
+    const res = await fetch('https://ddragon.leagueoflegends.com/cdn/14.6.1/data/zh_TW/champion.json');
+    const data = await res.json();
+    champions = Object.values(data.data);
+    nextQuestion();
+}
+
+async function getRandomSkinUrl(championId) {
+    const res = await fetch(`https://ddragon.leagueoflegends.com/cdn/14.6.1/data/zh_TW/champion/${championId}.json`);
+    const data = await res.json();
+    const skins = data.data[championId].skins;
+    const randomSkin = skins[Math.floor(Math.random() * skins.length)];
+    return {
+        url: `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_${randomSkin.num}.jpg`,
+        skinNum: randomSkin.num
+    };
+}
+
+function randomEdgePosition() {
+    const edgePositions = [
+        [Math.random() * 10, Math.random() * 80 + 10],      
+        [90 + Math.random() * 10, Math.random() * 80 + 10], 
+        [Math.random() * 80 + 10, Math.random() * 10],      
+        [Math.random() * 80 + 10, 90 + Math.random() * 10]  
+    ];
+    const [posX, posY] = edgePositions[Math.floor(Math.random() * edgePositions.length)];
+    return `${posX}% ${posY}%`;
+}
+
+let correctChampion, correctSkinUrl;
+
+async function nextQuestion() {
+    document.getElementById('result-overlay').classList.remove('show');
+
+    const shuffled = champions.sort(() => 0.5 - Math.random());
+    const options = shuffled.slice(0, 4);
+    correctChampion = options[Math.floor(Math.random() * options.length)];
+
+    const skinData = await getRandomSkinUrl(correctChampion.id);
+    correctSkinUrl = skinData.url;
+
+    const imageContainer = document.getElementById('image-container');
+    imageContainer.style.backgroundImage = `url('${correctSkinUrl}')`;
+    imageContainer.style.backgroundPosition = randomEdgePosition();
+
+    const optionsContainer = document.getElementById('options-container');
+    optionsContainer.innerHTML = '';
+
+    options.forEach(option => {
+        const btn = document.createElement('button');
+        btn.innerText = option.name;
+        btn.onclick = () => showResult(option.id === correctChampion.id);
+        optionsContainer.appendChild(btn);
+    });
+}
+
+function showResult(isCorrect) {
+    const overlay = document.getElementById('result-overlay');
+    document.getElementById('full-image').src = correctSkinUrl;
+    document.getElementById('result-text').innerHTML = isCorrect
+        ? `🎉 答對了！是【${correctChampion.name}】`
+        : `❌ 答錯了，正確答案是【${correctChampion.name}】`;
+
+    overlay.classList.add('show');
+}
+
+document.getElementById('continue-btn').onclick = nextQuestion;
+
+loadChampions();
